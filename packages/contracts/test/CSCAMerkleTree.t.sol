ceda025888e6fef3fa9561de8530642db007c1f6
Yoshi <yoshizou.tanaka@gmail.com>
2026-04-09 01:39:11 +0900

    update pragma

--- packages/contracts/test/CSCAMerkleTree.t.sol
+++ packages/contracts/test/CSCAMerkleTree.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 import {CSCAMerkleTree} from "../src/CSCAMerkleTree.sol";
