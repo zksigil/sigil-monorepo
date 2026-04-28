3f0ba1b5919d1bafa72b70c854571ea0ca87fe14
Yoshi <yoshizou.tanaka@gmail.com>
2026-04-28 15:46:22 +0900

    fixing pragma sol

--- packages/contracts/src/CSCAMerkleTree.sol
+++ packages/contracts/src/CSCAMerkleTree.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
--- packages/contracts/src/interfaces/ICSCAMerkleTree.sol
+++ packages/contracts/src/interfaces/ICSCAMerkleTree.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 /// @title ICSCAMerkleTree
 /// @notice Interface for the on-chain CSCA Merkle tree root registry.
